9d2f304e8bc5e2bbb3fc3847b950b95e6253f1a0
Anton Garay <antonvince.garay@gmail.com>
2024-07-28 16:09:25 +1200

    Fixed padding and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Anton's Frontendmentor Submissions!
 
-Hello this is my repository for all of my Frontendmentor submissions! I'm currently using React, Tailwindcss and Vite to try and recreate these pages!
+Hello this is my repository for all of my Frontendmentor submissions! I'm currently using React, Tailwindcss, framer-motion and Vite to try and recreate these pages! I chose to do Frontendmentor as I'd like to learn front end, Typescript and React.
+
+# Challenges done so far
+
+- [QRCODE](https://www.frontendmentor.io/solutions/qr-component-with-react-tailwind-and-vite-qNmAoFBVrJ)
+- [Social Profile](https://www.frontendmentor.io/solutions/social-profile-page-with-react-tailwind-and-vite-0ZXFKmD8Ai)
+- [Dessert List with Cart](https://www.frontendmentor.io/solutions/dessert-list-with-react-tailwindcss-and-vite-nAghZxSZe8)
+- [Product Preview Card](https://www.frontendmentor.io/solutions/responsive-product-preview-card-with-react-tailwindcss-and-vite-dAPmwDm56f)
--- src/Pages/Submissions/ProductPreviewCard/Productpreview.tsx
+++ src/Pages/Submissions/ProductPreviewCard/Productpreview.tsx
@@ -23,35 +23,37 @@ function ProductPreview() {
                 />
               </picture>
             </div>
-            <div className="flex h-full w-full flex-col p-8 text-[12px]">
-              <p className="Montserrat tracking-[4px] text-[#88878F]">
-                PERFUME
-              </p>
-              <p className="Fraunces mt-5 text-[32px] font-bold leading-8">
-                Gabrielle Essence Eau De Parfum
-              </p>
-              <p className="Montserrat mt-6 text-[14px] text-[#88878F]">
-                A floral, solar and voluptuous interpretation composed by
-                Olivier Polge, Perfumer-Creator for the House of CHANEL.
-              </p>
-              <div className="mt-6 flex flex-row items-center">
-                <p className="Fraunces text-[32px] font-bold text-[#3C8268]">
-                  $149.99
+            <div className="flex h-full w-full flex-col text-[12px]">
+              <div className="p-8">
+                <p className="Montserrat tracking-[4px] text-[#88878F]">
+                  PERFUME
                 </p>
-                <p className="Montserrat ml-5 text-[14px] text-[#81848B] line-through">
-                  $169.99
+                <p className="Fraunces mt-5 text-[32px] font-bold leading-8">
+                  Gabrielle Essence Eau De Parfum
                 </p>
+                <p className="Montserrat mt-6 text-[14px] text-[#88878F]">
+                  A floral, solar and voluptuous interpretation composed by
+                  Olivier Polge, Perfumer-Creator for the House of CHANEL.
+                </p>
+                <div className="mt-6 flex flex-row items-center">
+                  <p className="Fraunces text-[32px] font-bold text-[#3C8268]">
+                    $149.99
+                  </p>
+                  <p className="Montserrat ml-5 text-[14px] text-[#81848B] line-through">
+                    $169.99
+                  </p>
+                </div>
+                <motion.button
+                  className="mt-5 flex h-[45px] w-[213px] items-center justify-center rounded-xl bg-[#3D8168]"
+                  whileHover={{ scale: 1.1, backgroundColor: '#1A4031' }}
+                  whileTap={{ scale: 0.95 }}
+                >
+                  <img src="ProductPreviewCard/icon-cart.svg" />
+                  <p className="Montserrat mx-2 font-bold text-white">
+                    Add to Cart
+                  </p>
+                </motion.button>
               </div>
-              <motion.button
-                className="mt-5 flex h-[45px] w-[213px] items-center justify-center rounded-xl bg-[#3D8168]"
-                whileHover={{ scale: 1.1, backgroundColor: '#1A4031' }}
-                whileTap={{ scale: 0.95 }}
-              >
-                <img src="ProductPreviewCard/icon-cart.svg" />
-                <p className="Montserrat mx-2 font-bold text-white">
-                  Add to Cart
-                </p>
-              </motion.button>
             </div>
           </motion.div>
         </div>
